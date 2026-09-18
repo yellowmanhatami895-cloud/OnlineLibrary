82212cf034aa299332b0303aff86436cdf9a4f0a
Mohammad Javad <yellowmanhatami895@gmail.com>
2026-09-18 10:58:45 +0330

    better author sql

--- Author.go
+++ Author.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func deleteOwnedBook(bookID int, userID int) {
@@ -11,53 +10,31 @@ func deleteOwnedBook(bookID int, userID int) {
 
 func EditOwnedBooks(userID int, bookID int) {
 	printBooksByID(bookID)
-	record := getInputs([]string{"Enter new title", "Enter new description", "Enter new price", "Enter new category_id"})
-	price, err := strconv.Atoi(record[2])
-	if err != nil {
-		fmt.Println(err)
+	fmt.Println("**** Enter new row ****")
+	i, title, description, price, authorID, categoryID := getBookRow()
+	if i == 0 {
 		return
 	}
-
-	categoryID, err := strconv.Atoi(record[3])
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	db.Exec("UPDATE books SET title = ?,description = ?,price = ?,category_id = ? WHERE id = ? AND author_id = ?", record[0], record[1], price, categoryID, bookID, userID)
+	db.Exec("UPDATE books SET title = ?,description = ?,price = ?,category_id = ? WHERE id = ? AND author_id = ?", title, description, price, authorID, categoryID)
 }
 func printSales(userID int) {
-	var bookIDs []int
-	var totalSales int
-	var price int
-	bookSalesMap := make(map[int]int)
-
-	row, err := db.Query("SELECT id FROM books WHERE author_id = ?", userID)
+	sales, err := db.Query("SELECT order_items.price,books.title FROM order_items INNER JOIN books ON books.id = order_items.book_id INNER JOIN orders ON order_items.order_id = orders.id WHERE books.author_id = ? AND orders.status = 'paid'", userID)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	var bookID int
-	for row.Next() {
-		row.Scan(&bookID)
-		bookIDs = append(bookIDs, bookID)
+	var totalPrice int
+	var bookTitle string
+	for sales.Next() {
+		var price int
+		sales.Scan(&price, &bookTitle)
+		totalPrice = totalPrice + price
+		fmt.Printf("---Name : %s  | Price : %d ---\n", bookTitle, price)
 	}
-	for _, i := range bookIDs {
-		var bookSales int
-		var bookName string
-		row, err := db.Query("SELECT price FROM order_items WHERE book_id = ?", i)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		for row.Next() {
-			row.Scan(&price)
-			totalSales = totalSales + price
-			bookSales = bookSales + price
-		}
-		bookSalesMap[i] = bookSales
-		db.QueryRow("SELECt title FROM books WHERE id = ?", i).Scan(&bookName)
-		fmt.Printf("----[Name : %s Sales : %d ]----\n", bookName, bookSales)
+	err = sales.Err()
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-	fmt.Printf("----------Total sales : %d----------\n", totalSales)
-
+	fmt.Printf(" ------ Total Sales %d ------\n", totalPrice)
 }
